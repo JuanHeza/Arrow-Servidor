8bf24a77100ff92d9ba9970aec10deeb01bd875c
JuanHeza <juanehza@hotmail.com>
2020-09-10 14:24:28 -0500

    prueba con el bot semicompleto fix #4

--- app/controllers/telegram_bots_controller.rb
+++ app/controllers/telegram_bots_controller.rb
@@ -13,13 +13,13 @@ class TelegramBotsController < ApplicationController
     @message = Telegram::Bot::Types::Update.new(data).message
     pp data
     pp "Message received: #{@message.inspect}"
-    case message
+    case @message
     when Telegram::Bot::Types::CallbackQuery
-      puts message.data
-      puts message.message.text
-      bot.api.send_message(chat_id: message.from.id, text: "Serie #{message.data} iniciada")
+      puts @message.data
+      puts @message.message.text
+      bot.api.send_message(chat_id: @message.from.id, text: "Serie #{@message.data} iniciada")
     when Telegram::Bot::Types::Message
-      case message.text
+      case @message.text
       when "/start"
         list = []
         for i in 0..Comands.size
@@ -29,13 +29,13 @@ class TelegramBotsController < ApplicationController
           end
         end
         answers = Telegram::Bot::Types::ReplyKeyboardMarkup.new(keyboard: list, one_time_keyboard: true)
-        bot.api.send_message(chat_id: message.chat.id, text: "Bienvenido aqui puedes controlar el estado de las alertas del sistema :)", reply_markup: answers)
+        bot.api.send_message(chat_id: @message.chat.id, text: "Bienvenido aqui puedes controlar el estado de las alertas del sistema :)", reply_markup: answers)
       when "/status"
-        bot.api.send_message(chat_id: message.chat.id, text: "La Alerta esta #{AlertsController.alertStatus} y se esta ejecutando la secuencia: #{AlertsController.actualSerie}")
+        bot.api.send_message(chat_id: @message.chat.id, text: "La Alerta esta #{AlertsController.alertStatus} y se esta ejecutando la secuencia: #{AlertsController.actualSerie}")
       when "/alerttoggle"
         AlertsController.alertstatus = AlertsController.alertstatus
       when "/getserie"
-        bot.api.send_message(chat_id: message.chat.id, text: "La serie actual es: #{alertstatus.actualSerie}")
+        bot.api.send_message(chat_id: @message.chat.id, text: "La serie actual es: #{alertstatus.actualSerie}")
         #implementar un gif de la serie
       when "/setSerie"
         kb = []
@@ -44,12 +44,12 @@ class TelegramBotsController < ApplicationController
           kb.push(Telegram::Bot::Types::InlineKeyboardButton.new(text: alert.titulo, callback_data: alert.id))
         end
         answers = Telegram::Bot::Types::InlineKeyboardMarkup.new(inline_keyboard: kb)
-        bot.api.send_message(chat_id: message.chat.id, text: "Series:", reply_markup: answers)
+        bot.api.send_message(chat_id: @message.chat.id, text: "Series:", reply_markup: answers)
       when "/stop"
         kb = Telegram::Bot::Types::ReplyKeyboardRemove.new(remove_keyboard: true)
-        bot.api.send_message(chat_id: message.chat.id, text: "Bye, #{message.from.username}", reply_markup: kb)
+        bot.api.send_message(chat_id: @message.chat.id, text: "Bye, #{@message.from.username}", reply_markup: kb)
       else
-        bot.api.send_message(chat_id: message.chat.id, text: "Lo siento #{message.from.username}, Comando no reconocido.")
+        bot.api.send_message(chat_id: @message.chat.id, text: "Lo siento #{@message.from.username}, Comando no reconocido.")
       end
     end
   end
