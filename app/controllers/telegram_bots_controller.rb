97741dff3eda54182637e3eb1fc989dc29ec136c
JuanHeza <juanehza@hotmail.com>
2020-09-09 19:33:57 -0500

    webhooks fix #1.5

--- app/controllers/telegram_bots_controller.rb
+++ app/controllers/telegram_bots_controller.rb
@@ -1,4 +1,4 @@
-class TelegramBotController < ApplicationController
+class TelegramBotsController < ApplicationController
   def test
     request.body.rewind
     data = JSON.parse(request.body.read)
