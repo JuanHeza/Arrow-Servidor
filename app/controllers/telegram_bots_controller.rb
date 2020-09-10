5577633e7ce08f37e35078e9bf1345644cec9ebc
JuanHeza <juanehza@hotmail.com>
2020-09-09 19:30:36 -0500

    webhooks fix #1

--- app/controllers/telegram_bots_controller.rb
+++ app/controllers/telegram_bots_controller.rb
@@ -2,6 +2,9 @@ class TelegramBotController < ApplicationController
   def test
     request.body.rewind
     data = JSON.parse(request.body.read)
+    @bot = Telegram::Bot::Api.new(ENV['TELEGRAM_BOT_API_TOKEN'])
+    @message = Telegram::Bot::Types::Update.new(data).message
     pp data
+    pp "Message received: #{@message.inspect}"
   end
 end
